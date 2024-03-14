ab770c9b781ec3638be4ed24fcbc21e55bf2c922
Roberto Reta <robbie.reta67@gmail.com>
2024-03-14 18:24:01 +0000

    .

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,33 +1,33 @@
-import type { NextRequest } from 'next/server'
-import { NextResponse } from 'next/server'
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
-type Middleware = (request: NextRequest) => NextResponse
+type Middleware = (request: NextRequest) => NextResponse;
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const redirectIfAuthenticated: Middleware = (request) => {
-  const authSession = request.cookies.get('auth')?.value
+  const authSession = request.cookies.get("auth")?.value;
 
   if (authSession) {
-    return NextResponse.redirect(new URL('/', request.url))
+    return NextResponse.redirect(new URL("/", request.url));
   }
 
-  return NextResponse.next()
-}
+  return NextResponse.next();
+};
 
 const authenticated: Middleware = (request) => {
-  const authSession = request.cookies.get('auth')?.value
+  const authSession = request.cookies.get("auth")?.value;
 
   if (!authSession) {
-    const response = NextResponse.redirect(new URL('/login', request.url))
+    const response = NextResponse.redirect(new URL("/login", request.url));
     response.cookies.set({
-      name: 'redirect',
+      name: "redirect",
       value: request.url,
-    })
-    return response
+    });
+    return response;
   }
 
-  return NextResponse.next()
-}
+  return NextResponse.next();
+};
 
 export default function middleware(request: NextRequest) {
   // Uncomment if you want to redirect if authenticated.
@@ -38,12 +38,9 @@ export default function middleware(request: NextRequest) {
   //   return redirectIfAuthenticated(request)
   // }
 
-  if ([
-    '/',
-    '/pokemons',
-  ].includes(request.nextUrl.pathname)) {
-    return authenticated(request)
+  if (["/"].includes(request.nextUrl.pathname)) {
+    return authenticated(request);
   }
 
-  return NextResponse.next()
+  return NextResponse.next();
 }
